e03365cc6149bd85f42d24be1b8d33d73973e393
Craig Andera <candera@wangdera.com>
2018-06-07 07:03:43 -0400

    ACP module for ATO admittance.

--- acp/ato_admittance_module.php
+++ acp/ato_admittance_module.php
@@ -52,7 +52,7 @@ class ato_admittance_module
                 trigger_error('FORM_INVALID');
             }
 
-            $sql = "INSERT INTO {$ato_table_prefix}admittance (Name, Active) VALUES ('Change Me', b'1');";
+            $sql = "INSERT INTO {$ato_table_prefix}admittance (Name, Active) VALUES ('Change Me', b'0');";
 
             $db->sql_freeresult($this->execute_sql($sql));
         }
@@ -63,7 +63,72 @@ class ato_admittance_module
                 trigger_error('FORM_INVALID');
             }
 
-            // TODO
+            error_log("updating admittance");
+
+            $categories_submitted = $request->variable("category", array(0 => array("" => "")));
+            $categories_initial = $request->variable("category-initial", array(0 => array("" => "")));
+
+            // This is a little gross - so many separate updates - but
+            // the alternative is a giant SQL statement with a lot of
+            // CASE clauses.
+            foreach ($categories_initial as $adm_id => $init_info)
+            {
+                $initial_name = $init_info["Name"];
+                $submitted_name = $categories_submitted[$adm_id]["Name"];
+
+                error_log("Comparing name '{$initial_name}' with '{$submitted_name}' for {$adm_id}");
+
+                if ($initial_name != $submitted_name)
+                {
+                    $db->sql_freeresult(
+                        $this->execute_sql(
+                            "UPDATE {$ato_table_prefix}admittance SET Name = '" .
+                            $db->sql_escape($submitted_name) .
+                            "' WHERE Id = {$adm_id}"));
+                }
+
+                $initial_active = $init_info["Active"] == "true" ? true : false;
+                $submitted_active = $categories_submitted[$adm_id]["Active"] == "on" ? true : false;
+
+                error_log("Comparing active '{$initial_active}' with '{$submitted_active}' for {$adm_id}");
+
+                if ($initial_active != $submitted_active)
+                {
+                    $newval = $submitted_active ? "1" : "0";
+                    $db->sql_freeresult(
+                        $this->execute_sql(
+                            "UPDATE {$ato_table_prefix}admittance SET Active = b'{$newval}' WHERE Id = {$adm_id}"));
+                }
+
+            }
+
+            $admittance_submitted = $request->variable("admittances", array(0 => array(0 => "")));
+            $admittance_initial = $request->variable("admittance-initial", array(0 => array(0 => "")));
+
+            foreach ($admittance_initial as $adm_id => $init_info)
+            {
+                foreach ($init_info as $group_id => $group_initial)
+                {
+                    $initial_val = $group_initial == "true" ? true : false;
+                    $submitted_val = $admittance_submitted[$adm_id][$group_id] == "on" ? true : false;
+
+                    if ($initial_val != $submitted_val)
+                    {
+                        if ($submitted_val)
+                        {
+                            $db->sql_freeresult(
+                                $this->execute_sql(
+                                    "INSERT INTO {$ato_table_prefix}admittance_groups (AdmittanceId, GroupId) VALUES ({$adm_id}, {$group_id})"));
+                        }
+                        else
+                        {
+                            $db->sql_freeresult(
+                                $this->execute_sql(
+                                    "DELETE FROM {$ato_table_prefix}admittance_groups WHERE AdmittanceId = {$adm_id} AND GroupId = {$group_id}"));
+                        }
+                    }
+                }
+            }
         }
 
         $user->add_lang('acp/common');
@@ -72,61 +137,35 @@ class ato_admittance_module
         $this->page_title = $user->lang('ACP_VFW440_ATO_ADMITTANCE_TITLE');
 
         $sql = "SELECT Id, Name, Active FROM {$ato_table_prefix}admittance";
-
         $result = $this->execute_sql($sql);
-
         while ($row = $db->sql_fetchrow($result))
         {
             $template->assign_block_vars("admittance", $row);
         }
-
         $db->sql_freeresult($result);
 
-        $sql = "SELECT group_id as Id, group_name as Name FROM " . GROUPS_TABLE;
-
+        $sql = "SELECT group_id as Id, group_name as Name FROM " . GROUPS_TABLE . " ORDER BY Name";
         $result = $this->execute_sql($sql);
-
         while ($row = $db->sql_fetchrow($result))
         {
             $template->assign_block_vars("groups", $row);
-
-            $result2 = $this->execute_sql("SELECT GroupId FROM {$ato_table_prefix}admittance_groups WHERE AdmittanceId = " . $row["Id"]);
-
-            while ($row2 = $db->sql_fetchrow($result2))
-            {
-                $template->assign_block_vars("admittance.groups", $row2);
-            }
-
-            $db->sql_freeresult($result2);
         }
-
         $db->sql_freeresult($result);
 
-        // $sql = "select "
-        //      . implode(", ", $schema["columns"])
-        //      . " from "
-        //      . $schema["table"];
+        $sql = "SELECT AdmittanceId, GroupId FROM {$ato_table_prefix}admittance_groups";
+        $result = $this->execute_sql($sql);
+        $admittance_groups = [];
+        while ($row = $db->sql_fetchrow($result))
+        {
+            $admittance_groups[$row["AdmittanceId"]][$row["GroupId"]] = true;
+        }
+        $db->sql_freeresult($result);
 
-        // $result = $this->execute_sql($sql);
-
-        // $template->assign_vars(["TITLE" =>  $schema["title"]]);
-
-        // while ($row = $db->sql_fetchrow($result))
-        // {
-        //     $template->assign_block_vars('tablerow', $row);
-        // }
-
-        // $db->sql_freeresult($result);
-
-        // foreach ($schema["column-types"] as $colname => $coltype)
-        // {
-        //     $template->assign_block_vars('tablecol', array("Name" => $colname,
-        //                                                    "Type" => $coltype));
-        // }
+        $template->assign_var("admittance_groups", $admittance_groups);
 
         add_form_key('VFW440/flight_management');
     }
-    
+
     function main($id, $mode)
     {
         if ($mode == "admittance")
@@ -138,7 +177,7 @@ class ato_admittance_module
             trigger_error('Unknown mode {$mode}');
             return;
         }
-            
+
 
     }
 
